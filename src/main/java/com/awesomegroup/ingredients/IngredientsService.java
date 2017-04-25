983bfccd411a182ecafa16d438e892ad52e21122
Filip Wróbel <wrobelfilip@gmail.com>
2017-04-25 14:54:12 +0200

    [+] Some base classes for ingredients

---
+++ src/main/java/com/awesomegroup/ingredients/IngredientsService.java
@@ -0,0 +1,14 @@
+package com.awesomegroup.ingredients;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by wrobe on 25.04.2017.
+ */
+
+@Service
+@Transactional
+public class IngredientsService {
+}
