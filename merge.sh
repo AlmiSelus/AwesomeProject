5d8cfef8954ffb87790f85f6c2edce95e745bb50
almiselus <michal.stasiak.92@gmailc.om>
2017-05-23 20:20:14 +0200

    Added merge.sh [ci skip]

---
+++ merge.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+GIT_USER="$1"
+GIT_PASS="$2"
+
+# Specify the development branch and stable branch names
+FROM_BRANCH="$3"
+TO_BRANCH="$4"
+
+# Get the current branch
+export PAGER=cat
+CURRENT_BRANCH=$(git log -n 1 --pretty=%d HEAD | cut -d"," -f3 | cut -d" " -f2 | cut -d")" -f1)
+echo "current branch is '$CURRENT_BRANCH'"
+
+# Create the URL to push merge to
+URL=$(git remote -v | head -n1 | cut -f2 | cut -d" " -f1)
+echo "Repo url is $URL"
+PUSH_URL="https://$GIT_USER:$GIT_PASS@${URL:6}"
+
+if [ "$CURRENT_BRANCH" = "$FROM_BRANCH" ] ; then
+    # Checkout the FROM branch
+    #git checkout $FROM_BRANCH && \
+    #echo "Checking out $TO_BRANCH..." && \
+
+    git config --global merge.ours.driver true && \
+
+    # Checkout the latest TO branch
+    git fetch origin $TO_BRANCH:$TO_BRANCH && \
+    git checkout $TO_BRANCH && \
+
+    # Merge the dev into latest stable
+    echo "Merging changes..." && \
+    git merge $FROM_BRANCH && \
+
+    # Push changes back to remote vcs
+    echo "Pushing changes..." && \
+    git push $PUSH_URL && \
+    echo "Merge complete!" || \
+    echo "Error Occurred. Merge failed"
+else
+    echo "Not on $FROM_BRANCH. Skipping merge"
+fi
