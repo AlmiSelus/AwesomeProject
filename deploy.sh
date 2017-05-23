a165ac5c06e5be54ff902e20c58db7b48c20eac2
almiselus <michal.stasiak.92@gmailc.om>
2017-05-23 19:25:49 +0200

    Master changes in .travis.yml

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rev=$(git rev-parse --short HEAD)
-git config user.name "JanuszCebulaJ"
-git config user.password "OnionJava"
-git add .
-git commit -m "Committed automatically by Travis at ${rev}"
-git push origin HEAD:$1
