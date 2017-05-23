d31704e772fe2fb9ed3ff3df5161c5669369e61b
almiselus <michal.stasiak.92@gmailc.om>
2017-05-23 19:24:18 +0200

    Build changes

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ rev=$(git rev-parse --short HEAD)
 git config user.name "JanuszCebulaJ"
 git config user.password "OnionJava"
 git add .
+git reset .travis.yml
 git commit -m "Committed automatically by Travis at ${rev}"
 git push origin HEAD:$1
