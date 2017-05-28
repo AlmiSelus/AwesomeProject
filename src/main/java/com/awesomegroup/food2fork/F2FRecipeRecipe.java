5876da1d297e6107bf9c3f2b29297aa6deb5b8a6
SineR <patryk.kochanski@hotmail.com>
2017-05-28 18:45:17 +0200

    [*] SonarQubue fix test (problems with accessors)

--- src/main/java/com/awesomegroup/food2fork/F2FRecipeRecipe.java
+++ src/main/java/com/awesomegroup/food2fork/F2FRecipeRecipe.java
@@ -1,8 +1,12 @@
 package com.awesomegroup.food2fork;
 
-public class F2FRecipeRecipe
-{
-    public F2FRecipeResult recipe;
+public class F2FRecipeRecipe {
+    private F2FRecipeResult recipe;
+
+    public F2FRecipeResult GetRecipe()
+    {
+        return recipe;
+    }
 
     public String toString()
     {
